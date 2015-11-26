a08c3a9dec2f038a7b6d235e2778f214d3cdd789
Dominic <meiserdo@web.de>
2015-11-26 18:57:26 +0100

    fix for qt below 5.5

--- httpserver/httpglobal.h
+++ httpserver/httpglobal.h
@@ -9,19 +9,24 @@
 #include <QtGlobal>
 
 // This is specific to Windows dll's
-#if defined(Q_OS_WIN)
-	#if defined(QTWEBAPPLIB_EXPORT)
-		#define DECLSPEC Q_DECL_EXPORT
-	#elif defined(QTWEBAPPLIB_IMPORT)
-		#define DECLSPEC Q_DECL_IMPORT
-	#endif
+#ifdef Q_OS_WIN
+#  if defined QTWEBAPPLIB_EXPORT
+#    define DECLSPEC Q_DECL_EXPORT
+#  elif defined QTWEBAPPLIB_IMPORT
+#    define DECLSPEC Q_DECL_IMPORT
+#  endif
 #endif
-#if !defined(DECLSPEC)
-	#define DECLSPEC
+#ifndef DECLSPEC
+#  define DECLSPEC
 #endif
 
 /** Get the library version number */
 DECLSPEC const char *getQtWebAppLibVersion();
+
+// fix for qt versions below 5.5
+#ifndef Q_ENUM
+#  define Q_ENUM(x) Q_ENUMS(x)
+#endif
 
 #endif // HTTPGLOBAL_H
 
